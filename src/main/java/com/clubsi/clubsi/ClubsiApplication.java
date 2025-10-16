5310716968387897d287c9d8a7e9e76d88292147
Rohan <rohanwani10032006@gmail.com>
2025-10-16 08:32:02 +0530

    initial commit

---
+++ src/main/java/com/clubsi/clubsi/ClubsiApplication.java
@@ -0,0 +1,13 @@
+package com.clubsi.clubsi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubsiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClubsiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/clubsi/clubsi/ClubsiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clubsi.clubsi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubsiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
